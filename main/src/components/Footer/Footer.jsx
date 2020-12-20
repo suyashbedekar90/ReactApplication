7e5c33fa876354b7d2c227c8ff82fb947eaf5344
SuyashBedekar <suyash.bedekar@target.com>
2020-12-20 11:51:42 -0600

    footer container setup and styling

--- main/src/components/Footer/Footer.jsx
+++ main/src/components/Footer/Footer.jsx
@@ -5,7 +5,12 @@ const Footer = () => {
     return (
         <div className="footer_wrapper">
             <h1>How It Works</h1>
-            <div>Option values</div>
+            <div className="footer_content_wrapper">
+                <div className="footer_data">One</div>
+                <div className="footer_data">Two</div>
+                <div className="footer_data">Three</div>
+                <div className="footer_data">Four</div>
+            </div>
         </div>
     )
 }

1012b50984ee59b2743f88f13f3c7caf36d2162f
SuyashBedekar <suyash.bedekar@target.com>
2020-12-20 13:36:40 -0600

    adding localState to fetch and store API date

--- main/src/components/Footer/Footer.jsx
+++ main/src/components/Footer/Footer.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import '../../styles/Footer.css'
 
 const Footer = () => {
+    let [footerData, setFooterData] = useState([]);
+
+    useEffect(() => {
+        axios.get(`https://uqnzta2geb.execute-api.us-east-1.amazonaws.com/default/FrontEndCodeChallenge`)
+            .then(response => {
+                if (response !== undefined) {
+                    setFooterData(response.data)
+                }
+            })
+    }, [setFooterData]);
+
     return (
         <div className="footer_wrapper">
             <h1>How It Works</h1>
             <div className="footer_content_wrapper">
-                <div className="footer_data">One</div>
-                <div className="footer_data">Two</div>
-                <div className="footer_data">Three</div>
-                <div className="footer_data">Four</div>
+                {footerData.length > 0 &&
+                    footerData.map((data) => (
+                        <div className="footer_data">
+                            <span>{data.stepNumber}</span>
+                            <span><hr /></span>
+                            <span>{data.versionContent[0].title}</span>
+                            <span>{data.versionContent[0].body}</span>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
